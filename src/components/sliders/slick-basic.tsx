b1789424537cb67798fb915748c0e5321292baff
jmguevarra <jaimeguevarra22@gmail.com>
2025-01-13 06:55:11 +0800

    setup slick basic slider component

--- src/components/sliders/slick-basic.tsx
+++ src/components/sliders/slick-basic.tsx
@@ -1,14 +1,34 @@
 import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 interface props {
-  links: string[];
+  images: string[];
 }
 
-const SlickBasicSlider: React.FC<props> = ({ links }) => {
+const SlickBasicSlider: React.FC<props> = ({ images }) => {
+  //set configuration
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  };
+
   return (
-    <>
-      <div>{links[0]}</div>
-    </>
+    <div>
+      <Slider {...settings}>
+        {images.map((image, index) => (
+          <div key={index}>
+            <img src={image} alt={`Slide ${index}`} className="w-full h-auto" />
+          </div>
+        ))}
+      </Slider>
+    </div>
   );
 };
 
--- src/templates/home.tsx
+++ src/templates/home.tsx
@@ -1,15 +1,19 @@
 import { useContext } from "react";
 import WPContext from "../context/wp-context";
 import SlickBasicSlider from "../components/sliders/slick-basic";
+import { CarPost } from "../types/carpost";
 // import { CarPost } from "../types/carpost";
 
 const Home = () => {
   const { cars, page } = useContext(WPContext);
-  console.log(page, cars);
+  const { no_of_cars_in_hero_carousel } = page.acf?.page_settings ?? [];
 
-  // const imagesLinks = cars.map((car: CarPost) => car.featured_image_url);
+  //get the first 4 Car images
+  const imagesLinks = cars
+    .slice(0, no_of_cars_in_hero_carousel)
+    .map((car: CarPost) => car.featured_image_url);
 
-  return <SlickBasicSlider links={[]}></SlickBasicSlider>;
+  return <SlickBasicSlider images={imagesLinks}></SlickBasicSlider>;
 };
 
 export default Home;
