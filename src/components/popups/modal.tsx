5a9b06ead91049e75cc108822ede86ea7d40904c
jmguevarra <jaimeguevarra22@gmail.com>
2025-01-13 09:53:09 +0800

    config modal and added css component layer

--- src/components/popups/modal.tsx
+++ src/components/popups/modal.tsx
@@ -7,17 +7,24 @@ const Modal = () => {
 
   if (!isModalOpen) return null;
 
+  //closing modal
+  const onClose = () => {
+    setIsModalOpen(false);
+  };
+
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
-      <div className="bg-white p-6 rounded-lg shadow-lg w-96">
-        <button
-          className="absolute top-2 right-2 text-gray-500"
-          onClick={() => {
-            setIsModalOpen(false);
-          }}
-        >
+    <div
+      className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50"
+      onClick={onClose}
+    >
+      <div
+        className={`bg-white p-6 rounded-lg shadow-lg w-96 transition-all transform relative ${
+          isModalOpen ? "scale-100 translate-y-0" : "scale-95 translate-y-10"
+        }`}
+      >
+        <a className="modal-btn--closing" onClick={onClose}>
           &times;
-        </button>
+        </a>
         <div>{modalContent}</div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     './*.php',
     './**/*.php',
     './src/**/*.{js,jsx,ts,tsx}',
+    './wp-content/themes/twentytwentyfive-child/**/*.{php,js,jsx,ts,tsx}'
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
